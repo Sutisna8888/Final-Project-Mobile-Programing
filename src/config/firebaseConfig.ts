caae5a819cf6f2d90c3047fa90e6d3719bcb5895
Sutisna8888 <161696845+Sutisna8888@users.noreply.github.com>
2026-02-06 23:39:54 +0700

    Update Firebase configuration details
    Updated Firebase configuration with new credentials.

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -11,12 +11,12 @@ import { getFirestore } from "firebase/firestore";
 import { Platform } from "react-native";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDkubT5ovDWR5kkOucwb7vXMU8DfUTJfgI",
-  authDomain: "simplequizapp-d3f72.firebaseapp.com",
-  projectId: "simplequizapp-d3f72",
-  storageBucket: "simplequizapp-d3f72.firebasestorage.app",
-  messagingSenderId: "1002068454798",
-  appId: "1:1002068454798:web:fd2f2939df73a8841cc8df"
+  apiKey: "xxxxxxxxxxx",
+  authDomain: "xxxxxxxxxxx",
+  projectId: "xxxxxxxx",
+  storageBucket: "xxxxxxxxx",
+  messagingSenderId: "xxxxxxxx",
+  appId: "xxxxxxxxx"
 };
 
 const app = initializeApp(firebaseConfig);
